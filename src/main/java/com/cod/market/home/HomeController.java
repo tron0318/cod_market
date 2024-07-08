0225d02971698f09554cf298653b06c106167652
tron0318 <tron0318@naver.com>
2024-07-08 15:19:57 +0900

    home

---
+++ src/main/java/com/cod/market/home/HomeController.java
@@ -0,0 +1,16 @@
+package com.cod.market.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "홈 화면입니다.";
+    }
+}
