7f2188f4f092eada4e22573cb14627c9e5472e23
tron0318 <tron0318@naver.com>
2024-07-09 16:25:26 +0900

    password encode

--- src/main/java/com/cod/market/member/service/MemberService.java
+++ src/main/java/com/cod/market/member/service/MemberService.java
@@ -3,6 +3,7 @@ package com.cod.market.member.service;
 import com.cod.market.member.entity.Member;
 import com.cod.market.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -11,10 +12,11 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
     public Member join(String username, String password, String email, String nickname) {
         Member member = new Member();
         member.setUsername(username);
-        member.setPassword(password);
+        member.setPassword(passwordEncoder.encode(password));
         member.setEmail(email);
         member.setNickname(nickname);
         member.setCreateDate(LocalDateTime.now());
