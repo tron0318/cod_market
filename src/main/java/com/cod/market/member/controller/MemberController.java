0495e322ace66e63cd73fbcc4bbeb509a5a53a0f
tron0318 <tron0318@naver.com>
2024-07-09 16:14:25 +0900

    join

--- src/main/java/com/cod/market/member/controller/MemberController.java
+++ src/main/java/com/cod/market/member/controller/MemberController.java
@@ -1,13 +1,21 @@
 package com.cod.market.member.controller;
 
+import com.cod.market.member.form.MemberForm;
+import com.cod.market.member.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+    public final MemberService memberService;
+
 
     @GetMapping("/login")
     public String login(){
@@ -15,8 +23,16 @@ public class MemberController {
     }
 
     @GetMapping("/join")
-    public String join(){
+    public String showJoin(){
         return "member/join";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid MemberForm memberForm){
+        memberService.join(memberForm.getUsername(), memberForm.getPassword(), memberForm.getEmail(), memberForm.getNickname());
+
+        return "redirect:/member/login";
+
     }
 
 }
