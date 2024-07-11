4d17ea498106d440a6af4d09c3d204f9fc49b773
tron0318 <tron0318@naver.com>
2024-07-11 15:26:29 +0900

    modify&delete button authentication

--- src/main/java/com/cod/market/question/controller/QuestionController.java
+++ src/main/java/com/cod/market/question/controller/QuestionController.java
@@ -7,9 +7,12 @@ import com.cod.market.product.service.ProductService;
 import com.cod.market.question.entity.Question;
 import com.cod.market.question.service.QuestionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -21,45 +24,61 @@ public class QuestionController {
     private final ProductService productService;
     private final MemberService memberService;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String create(@PathVariable("id") Long id,
                          Principal principal,
-                         @RequestParam("content")String content){
+                         @RequestParam("content") String content) {
         Product product = productService.getProduct(id);
         Member member = memberService.findByUserName(principal.getName());
-        questionService.create(product,member,content);
+        questionService.create(product, member, content);
 
-        return String.format("redirect:/product/detail/%s",id);
+        return String.format("redirect:/product/detail/%s", id);
     }
+
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/modify/{id}")
-    public String modify(@PathVariable("id") Long id, Model model){
+    public String modify(@PathVariable("id") Long id, Model model, Principal principal) {
         Question question = questionService.getQuestion(id);
+
+        if (!question.getMember().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한 없음");
+        }
 
         model.addAttribute("question", question);
         return "question/modify";
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/modify/{id}")
     public String modify(@PathVariable("id") Long id,
-                         @RequestParam("content")String content){
+                         @RequestParam("content") String content, Principal principal) {
         Question question = questionService.getQuestion(id);
         questionService.modify(question, content);
         long productId = question.getProduct().getId();
 
+        if (!question.getMember().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한 없음");
+        }
 
-        return String.format("redirect:/product/detail/%s",productId);
+
+        return String.format("redirect:/product/detail/%s", productId);
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") Long id){
+    public String delete(@PathVariable("id") Long id, Principal principal) {
         Question question = questionService.getQuestion(id);
 
         questionService.delete(question);
         long productId = question.getProduct().getId();
 
-        return String.format("redirect:/product/detail/%s",productId);
-    }
+        if (!question.getMember().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제 권한 없음");
+        }
 
+        return String.format("redirect:/product/detail/%s", productId);
+    }
 
 
 }
