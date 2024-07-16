9766f1b6bcb398471d524b855e7eec6bee5dbe9c
tron0318 <tron0318@naver.com>
2024-07-16 14:39:35 +0900

    add member devInitData

--- src/test/java/com/cod/market/service/ProductServiceTests.java
+++ src/test/java/com/cod/market/service/ProductServiceTests.java
@@ -11,15 +11,15 @@ public class ProductServiceTests {
     @Autowired
     private ProductService productService;
 
-    @Test
-    @DisplayName("강의 제품 생성")
-    void test1(){
-        for ( int i = 1; i<= 200; i++){
-            String name = String.format("테스트 상품:[%03d]", i);
-            String description = String.format("테스트 설명:[%03d]", i);
-            int price = 100;
-            productService.create(name, description, price);
-        }
-    }
+//    @Test
+//    @DisplayName("강의 제품 생성")
+//    void test1(){
+//        for ( int i = 1; i<= 200; i++){
+//            String name = String.format("테스트 상품:[%03d]", i);
+//            String description = String.format("테스트 설명:[%03d]", i);
+//            int price = 100;
+//            productService.create(name, description, price);
+//        }
+//    }
 
 }
