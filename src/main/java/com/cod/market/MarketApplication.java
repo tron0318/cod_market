c66e16c017bcba6c057785b809f348d37a2afc2f
tron0318 <tron0318@naver.com>
2024-07-17 14:15:28 +0900

    tosspayments test code

--- src/main/java/com/cod/market/MarketApplication.java
+++ src/main/java/com/cod/market/MarketApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarketApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MarketApplication.class, args);
+    }
 
 }
